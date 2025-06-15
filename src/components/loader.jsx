16596adc5f5f5dbd0c01440c3c7aaccbb28b2f4f
Shivam Kumar <141110357+Shivam0713@users.noreply.github.com>
2025-06-16 01:51:48 +0530

    Update loader.jsx

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './Loader.css';
+import './loader.css';
 
-const Loader = () => (
-  <div className="loader-overlay">
-    <div className="loader"></div>
-  </div>
-);
+const Loader = ({ backgroundImage }) => {
+  return (
+    <div className="loader-overlay" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})` }}>
+      <div className="loader"></div>
+    </div>
+  );
+};
 
 export default Loader;
