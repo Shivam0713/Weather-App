84747b59fdc3cd74fe0fce06acef70c240190fb0
Shivam0713 <Shivamkumar0713@gmail.com>
2025-06-26 14:15:25 +0530

    Removing comment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Weather-App/', // <-- important for GitHub Pages
+  base: '/Weather-App/',
 });
